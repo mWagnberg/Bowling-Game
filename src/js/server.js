8c6d28b4ab333975e54ad52afff70c6e5ea1dfbc
Michael <michael@localhost.localdomain>
2018-06-12 09:16:17 +0200

    setup server and structure

---
+++ src/js/server.js
@@ -0,0 +1,46 @@
+'use strict'
+
+let express = require('express')
+let path = require('path')
+let app = express()
+let handlebars = require('express-handlebars')
+let bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+/**
+ * Use this to specify a path to static files
+ */
+app.use(express.static(path.join(__dirname, '../public')))
+
+/**
+ * This is for telling main is the default handlebar
+ */
+app.engine('.handlebars', handlebars({
+  defaultLayout: 'main',
+  extname: '.handlebars'
+}))
+
+app.set('view engine', '.handlebars')
+
+/**
+ * This is getting a index page, depending if a user is logged in or not
+ */
+app.get('/', function (request, response) {
+  response.render('index')
+})
+
+// Specify the port
+let port = process.env.port || 8000
+
+/**
+ * Start the server
+ */
+function startServer () {
+  app.listen(port, function () {
+    console.log('Listens to port: ' + port)
+  })
+}
+
+module.exports.startServer = startServer
