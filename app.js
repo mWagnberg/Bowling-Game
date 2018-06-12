188d97e7bcbae7e4c601d2f84eb3af15abffe338
Michael <michael@localhost.localdomain>
2018-06-12 08:42:56 +0200

    added functions for throwing ball and computation

--- app.js
+++ app.js
@@ -3,10 +3,24 @@
 let maxRandom = 11
 
 /**
- * This returns a number between 0 and 10
+ * This will run when throwing a bowling ball
+ * @param {*} n is the amount of possible pins that can be down
  */
-function randomNumber (n) {
-  console.log(Math.floor(Math.random() * n))
+function throwBowlingBall (n) {
+  let amountOfPins = Math.floor(Math.random() * n)
+  console.log('FIRST - ' + amountOfPins)
+  return amountOfPins
 }
 
-randomNumber(maxRandom)
+/**
+ * This will compute how many pins there are left from the first throw
+ * @param {*} n are the amount of pins
+ */
+function secondRound (n) {
+  let computation = maxRandom - n
+  let secondAmountOfPins = Math.floor(Math.random() * computation)
+  console.log('SECOND - ' + secondAmountOfPins)
+}
+
+let firstThrow = throwBowlingBall(maxRandom)
+secondRound(firstThrow)
